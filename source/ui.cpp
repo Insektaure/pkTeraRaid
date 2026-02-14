aefda70205696ba1cac6b4ce68b23c7c350b0910
Insektaure <41437620+Insektaure@users.noreply.github.com>
2026-02-14 18:32:29 +0100

    fix: correct formatting of switch case statements in UI event handling

--- source/ui.cpp
+++ source/ui.cpp
@@ -1517,9 +1517,11 @@ void UI::handleRaidViewInput(bool& running) {
 
             switch (event.key.keysym.sym) {
                 case SDLK_UP:
-                    if (count > 0) raidCursor_ = (raidCursor_ + count - 1) % count; break;
+                    if (count > 0) raidCursor_ = (raidCursor_ + count - 1) % count;
+                    break;
                 case SDLK_DOWN:
-                    if (count > 0) raidCursor_ = (raidCursor_ + 1) % count; break;
+                    if (count > 0) raidCursor_ = (raidCursor_ + 1) % count;
+                    break;
                 case SDLK_q: { // L
                     int m = ((int)currentMap_ + 2) % 3;
                     currentMap_ = (TeraRaidMapParent)m;
@@ -1535,7 +1537,8 @@ void UI::handleRaidViewInput(bool& running) {
                     break;
                 }
                 case SDLK_a: case SDLK_RETURN:
-                    if (count > 0) showDetail_ = true; break;
+                    if (count > 0) showDetail_ = true;
+                    break;
                 case SDLK_MINUS:
                     showAbout_ = true; break;
                 case SDLK_b: case SDLK_ESCAPE:
